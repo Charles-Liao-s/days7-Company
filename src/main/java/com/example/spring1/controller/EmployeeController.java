02a48e8ed8f69868ab0935038d0d906665223422
Charles Liao <charles.liao@oocl.com>
2025-09-09 16:18:30 +0800

    feat:EmployeeController and add filtering by gender in employee listing

--- src/main/java/com/example/spring1/controller/EmployeeController.java
+++ src/main/java/com/example/spring1/controller/EmployeeController.java
@@ -13,18 +13,33 @@ import java.util.Objects;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-  private List<Employee> employees = new ArrayList<>();
+    private List<Employee> employees = new ArrayList<>();
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public Employee create(@RequestBody Employee employee) {
-    Employee newEmployee = new Employee(employees.size() + 1, employee.name(), employee.age(), employee.gender(), employee.salary());
-    employees.add(newEmployee);
-    return newEmployee;
-  }
-  @GetMapping("/{id}")
-  @ResponseStatus(HttpStatus.OK)
-  public Employee getEmployeeById(@PathVariable Integer id) {
-    return employees.stream().filter(employee -> Objects.equals(employee.id(), id)).findFirst().orElse(null);
-  }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee create(@RequestBody Employee employee) {
+        Employee newEmployee = new Employee(employees.size() + 1, employee.name(), employee.age(), employee.gender(), employee.salary());
+        employees.add(newEmployee);
+        return newEmployee;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Employee get(@PathVariable Integer id) {
+        return employees.stream()
+                .filter(employee -> Objects.equals(employee.id(), id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void clear(){
+        employees.clear();
+    }
+
+    @GetMapping
+    public List<Employee> index(@RequestParam(required = false) String gender){
+        return employees.stream()
+                .filter(employee -> Objects.equals(employee.gender(), gender))
+                .toList();
+    }
 }
--- src/test/java/com/example/spring1/EmployeesTest.java
+++ src/test/java/com/example/spring1/EmployeesTest.java
@@ -7,6 +7,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.example.spring1.controller.Employee;
 import com.example.spring1.controller.EmployeeController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -24,6 +25,11 @@ public class EmployeesTest {
   private MockMvc mockMvc;
   @Autowired
   private EmployeeController employeeController;
+
+  @BeforeEach
+  public void setup() {
+    employeeController.clear();
+  }
 
   @Test
   public void should_return_created_employees_when_post() throws Exception {
@@ -53,12 +59,32 @@ public class EmployeesTest {
     MockHttpServletRequestBuilder request = get("/employees"+id).contentType(MediaType.APPLICATION_JSON);
     mockMvc.perform(request)
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.id").value(2))
-        .andExpect(jsonPath("$.name").value("John Smith"))
-        .andExpect(jsonPath("$.age").value(32))
-        .andExpect(jsonPath("$.gender").value("Male"))
-        .andExpect(jsonPath("$.salary").value(5000.0));
+        .andExpect(jsonPath("$.id").value(employee.id()))
+        .andExpect(jsonPath("$.name").value(employee.name()))
+        .andExpect(jsonPath("$.age").value(employee.age()))
+        .andExpect(jsonPath("$.gender").value(employee.gender()))
+        .andExpect(jsonPath("$.salary").value(employee.salary()));
   }
+
+  @Test
+  public void should_return_males_when_list_by_male() throws Exception {
+    Employee expect = employeeController.create(new Employee(null, "John", 32, "male", 5000.0));
+    employeeController.create(new Employee(null, "John", 32, "female", 5000.0));
+
+    MockHttpServletRequestBuilder request = get("/employees?gender=male")
+            .contentType(MediaType.APPLICATION_JSON);
+
+    mockMvc.perform(request)
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.length()").value(1))
+            .andExpect(jsonPath("$.[0].id").value(expect.id()))
+            .andExpect(jsonPath("$.[0].name").value(expect.name()))
+            .andExpect(jsonPath("$.[0].age").value(expect.age()))
+            .andExpect(jsonPath("$.[0].gender").value(expect.gender()))
+            .andExpect(jsonPath("$.[0].salary").value(expect.salary()));
+  }
+
+
 
 
 }
